f31ddac05551d3a412577d186599bebe99524b50
Sunandan <sunandan.bhakat2011@gmail.com>
2021-11-30 16:18:28 +0530

    Initial Commit

---
+++ src/main/java/com/filereader/LogFileReader/LogFileReaderApplication.java
@@ -0,0 +1,13 @@
+package com.filereader.LogFileReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogFileReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogFileReaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filereader/LogFileReader/LogFileReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filereader.LogFileReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogFileReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
